25400761b1a0cbb46f14b503015835a79406f1ec
fhelwanger <fhelwanger@gmail.com>
2020-07-14 22:27:33 -0300

    Create Route component

---
+++ src/components/Route.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import Box from "@material-ui/core/Box";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  title: {
+    paddingTop: theme.spacing(2),
+    paddingBottom: theme.spacing(2),
+  },
+}));
+
+export interface RouteProps {
+  title: string;
+  children?: React.ReactNode;
+}
+
+function Route({ title, children }: RouteProps) {
+  const classes = useStyles();
+
+  return (
+    <>
+      <Typography
+        variant="h3"
+        align="center"
+        paragraph
+        className={classes.title}
+      >
+        {title}
+      </Typography>
+      <Box maxWidth={800} margin="0 auto">
+        {children}
+      </Box>
+    </>
+  );
+}
+
+export default Route;
--- src/routes/any-nmg-normal/index.tsx
+++ src/routes/any-nmg-normal/index.tsx
@@ -1,20 +1,14 @@
 import React from "react";
-import Box from "@material-ui/core/Box";
-import Typography from "@material-ui/core/Typography";
+import Route from "../../components/Route";
 import Chapter1 from "./Chapter1";
 import Chapter2 from "./Chapter2";
 
 function AnyPercentNoMajorGlitchesNormal() {
   return (
-    <>
-      <Typography variant="h3" align="center" paragraph>
-        Any% No Major Glitches - Normal
-      </Typography>
-      <Box maxWidth={800} margin="0 auto">
-        <Chapter1 />
-        <Chapter2 />
-      </Box>
-    </>
+    <Route title="Any% No Major Glitches - Normal">
+      <Chapter1 />
+      <Chapter2 />
+    </Route>
   );
 }
 
