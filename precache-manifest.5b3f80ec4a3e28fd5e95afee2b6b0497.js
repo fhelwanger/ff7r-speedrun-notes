cc03512e9f7e3460eeb008dfa1cb9feb77f5e951
fhelwanger <fhelwanger@gmail.com>
2020-07-14 23:17:27 -0300

    Updates

---
+++ precache-manifest.5b3f80ec4a3e28fd5e95afee2b6b0497.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c98e8aa414d02e411bf9d15e42fd1394",
+    "url": "/ff7r-speedrun-notes/index.html"
+  },
+  {
+    "revision": "0e9493f9634d9ff70fd0",
+    "url": "/ff7r-speedrun-notes/static/js/2.282b4e5b.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/ff7r-speedrun-notes/static/js/2.282b4e5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5b88947ddb2b9435048",
+    "url": "/ff7r-speedrun-notes/static/js/main.e439cbe0.chunk.js"
+  },
+  {
+    "revision": "42d994a4b76930ef39b6",
+    "url": "/ff7r-speedrun-notes/static/js/runtime-main.12c092d6.js"
+  }
+]);
